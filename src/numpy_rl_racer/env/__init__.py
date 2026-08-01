e2b0a77111a6a9f9ded58930ceca6f788ccbf77a
Alexandre Edmond <alex@alexandres-mac-mini.home>
2026-08-01 19:13:36 +0200

    Resolve issue #149: Wire TrackPoolEnv into train.py for multi-track training

--- src/numpy_rl_racer/env/__init__.py
+++ src/numpy_rl_racer/env/__init__.py
@@ -1,6 +1,6 @@
 from .car import CarState, KinematicCar
 from .racing_env import Obstacle, ProceduralTrack, RacingEnv, reward_line_endpoints
-from .wrappers import ActionRepeatEnv, EpisodeMonitor
+from .wrappers import ActionRepeatEnv, EpisodeMonitor, TrackPoolEnv
 
 __all__ = [
     "ActionRepeatEnv",
@@ -10,5 +10,6 @@ __all__ = [
     "Obstacle",
     "ProceduralTrack",
     "RacingEnv",
+    "TrackPoolEnv",
     "reward_line_endpoints",
 ]
